cb1e6638f423dd715681a54b3ba0d7ddc036af78
Mostafa Essam <102423748+MrQuartz99@users.noreply.github.com>
2023-01-10 15:42:01 +0200

    Update getCurrency.js

--- src/functions/getCurrency.js
+++ src/functions/getCurrency.js
@@ -3,7 +3,12 @@ const CC = require('currency-converter-lt')
 async function getCurrency(firstCurrency , secondCurrency) {
     let currencyConverter = new CC({from:firstCurrency, to:secondCurrency, amount:1})
     let result = await currencyConverter.convert()
-    console.log(result)
+    if(result === undefined) return 0
+    if(result.toString().length === 4) {
+        let newResult = result.toString().slice(0,2) + "." + result.toString().slice(2,4)
+        return Number(newResult)
+    }
+
     return result
 }
 module.exports = getCurrency
